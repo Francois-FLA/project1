1cea1c469d0133d06184d385f88684c75e0c7868
surnova <francois.laffon.dev@gmail.com>
2017-09-04 18:40:59 +0200

    9eme commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,9 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 import { MenuComponent } from './menu/menu.component';
 import { InfosGeneralesComponent } from './infos-generales/infos-generales.component';
 import { InfosCandidatComponent } from './infos-candidat/infos-candidat.component';
-import { InfosCommentaireComponent } from './infos-commentaire/infos-commentaire.component';
-import { PagetotaleComponent } from './pagetotale/pagetotale.component';
 import { MenuRecruteursComponent } from './menu-recruteurs/menu-recruteurs.component';
 import { FormsComponent } from './forms/forms.component';
 import { CopyrightComponent } from './copyright/copyright.component';
@@ -18,13 +14,9 @@ import { CopyrightComponent } from './copyright/copyright.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
     MenuComponent,
     InfosGeneralesComponent,
     InfosCandidatComponent,
-    InfosCommentaireComponent,
-    PagetotaleComponent,
     MenuRecruteursComponent,
     FormsComponent,
     CopyrightComponent
